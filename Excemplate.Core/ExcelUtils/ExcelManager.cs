9a3629a6af94c67b60fcfadd433d6456488c6eb0
ikhramts <junkdump3001@hotmail.com>
2013-04-30 20:11:59 -0400

    Removed System.Threading.Tasks from ExcelManager.

--- Excemplate.Core/ExcelUtils/ExcelManager.cs
+++ Excemplate.Core/ExcelUtils/ExcelManager.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Excemplate.Core.ExcelUtils
