b7ea0a024690e162b09439e69a01c867b81e0b98
ikhramts <junkdump3001@hotmail.com>
2013-05-05 20:27:38 -0400

    Forgot to add ParserException.

---
+++ LanguageEvaluator/ParserException.fs
@@ -0,0 +1,30 @@
+﻿namespace Excemplate.Language
+
+open System
+
+type public ParserException(line:int, character:int, message:string, innerException:Exception) =
+    inherit Exception(message, innerException)
+    let mutable lineNum = line
+    let mutable charNum = character
+
+    (*************** Other Constructors *****************)
+    new() = ParserException(0, 0, "", null)
+    new(innerException) = ParserException(0, 0, "", innerException);
+    new(message, innerException) = ParserException(0, 0, message, innerException)
+    new(line, character, innerException) = ParserException(line, character, "", innerException)
+
+    (*************** Public Properties *****************)
+    member public this.LineNum
+        with get() = lineNum
+        and set value = 
+            lineNum <- value
+            ()
+
+    member public this.CharNum
+        with get() = charNum
+        and set value = 
+            charNum <- value
+            ()
+
+    
+    
